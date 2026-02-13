5b60b7415416876d0636d530e699b8a0064ade1e
arturocesar521-collab <arturocesar521@gmail.com>
2026-02-13 15:28:53 -0800

    descripcion de los cambios

--- sesion_03.py
+++ sesion_03.py
@@ -17,3 +17,18 @@ mi_lista_2 = ["lunes", "martes", "miercoles","jueves", "viernes"]
 for i in mi_lista_2:
    if i != "lunes":
       print(f" feliz {i}!")
+
+      
+
+      #While loop´
+      i = 0
+
+      while i < 5:
+         i += 1   
+         if i==3:
+            continue
+         print(i)
+         if i == 4:
+            break
+      else: 
+         print("i es ahora mayor o igual a >")
