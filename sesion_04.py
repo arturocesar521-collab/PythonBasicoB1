7f334f3fcad706eb8494b69981f9b6606baeb912
chrizmeza <angelariasmezacrack@gmail.com>
2026-02-16 22:44:28 -0800

    cambios del python recien editado

---
+++ sesion_04.py
@@ -0,0 +1,10 @@
+i = 0
+mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+
+i < 3
+i +=1
+
+for i in range (3):
+    for d in mi_lista_2:
+        if d != "lunes":
+           print(d)
